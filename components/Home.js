983cb83b19c5f38ff1e6a68991ef3205fd695930
Youness <youns@outlook.ie>
2023-09-22 18:36:03 +0200

    Add env and change fetch

--- components/Home.js
+++ components/Home.js
@@ -14,7 +14,7 @@ function Home() {
   const [topArticle, setTopArticle] = useState({});
 
   useEffect(() => {
-    fetch("http://localhost:3000/articles")
+    fetch(`${process.env.BACKEND_URL}/articles`)
       .then((response) => response.json())
       .then((data) => {
         setTopArticle(data.articles[0]);
